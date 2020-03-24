feb0a77b678d0ba8a46b0a73a679c329fd791348
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-03-24 22:49:40 +0300

    Updates

---
+++ precache-manifest.addb68b4356e0b6a09b9457851cb3fcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a4e4b2fe81eaa74efebd6500c879db",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "0637b171152d4030f59b",
+    "url": "/form/static/css/main.e6444f3e.chunk.css"
+  },
+  {
+    "revision": "7170f1987daa41e4c579",
+    "url": "/form/static/js/2.1a7f78b6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/form/static/js/2.1a7f78b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0637b171152d4030f59b",
+    "url": "/form/static/js/main.02fea2c3.chunk.js"
+  },
+  {
+    "revision": "3d7a5358e215a98d63dd",
+    "url": "/form/static/js/runtime-main.acf93c07.js"
+  }
+]);
